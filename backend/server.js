5161a951cf99df9faf0b1bcbc13980406c4201ce
AakarshPGL <gangalalaakarsh2006@gmail.com>
2026-08-14 16:45:31 +0530

    prepared backend for vercel

--- backend/server.js
+++ backend/server.js
@@ -572,10 +572,14 @@ app.post(
 // START SERVER
 // ===============================
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-    console.log(
-        `Server running on http://localhost:${PORT}`
-    )
-})
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log(
+            `Server running on http://localhost:${PORT}`
+        )
+    })
+}
+
+module.exports = app
